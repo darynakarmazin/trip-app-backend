7b9cb6199881197d36f4d3fbaae4ddf99a3116cb
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-02-19 15:58:50 +0200

    Added API for Google Authentication

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
 
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// API for Google Authentication
+app.post("/google-auth", async (req, res) => {
+  const { credential, client_id } = req.body;
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: credential,
+      audience: client_id,
+    });
+    const payload = ticket.getPayload();
+    console.log(ticket);
+    const userid = payload["sub"];
+    res.status(200).json({ payload });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 });
 
 app.listen(3000, () => {
